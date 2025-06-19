c92d0783a5eac61beaed08dc9b6e3995911440cc
Yash Ramesh Kumar <devyk100@gmail.com>
2025-06-19 16:19:57 +0530

    change URLs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://othersite.com',
+        permanent: true,
+      },
+      {
+        source: '/1',
+        destination: 'https://othersite.com/page-one',
+        permanent: true,
+      },
+    ];
+  },
+
 };
 
 export default nextConfig;
